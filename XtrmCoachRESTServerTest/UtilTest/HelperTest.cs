f9a5a24bb2c270d27deb742d505dc7d123cc4e34
atulbanwar <atulbanwar@gmail.com>
2016-10-30 14:46:31 -0400

    - Implemented Helper unit tests.

---
+++ XtrmCoachRESTServerTest/UtilTest/HelperTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using XtrmCoachRESTServer.Util;
+
+namespace XtrmCoachRESTServerTest.HelperTest
+{
+	[TestClass]
+	public class HelperTest
+	{
+		public class DummyClass
+		{
+			public string variable { get; set; }
+		}
+
+		[TestMethod]
+		public void Serialize_Test()
+		{
+			// Arrange
+			DummyClass dummyObj = new DummyClass();
+			dummyObj.variable = "value";
+			string serializedStr;
+
+			// Act
+			serializedStr = Helper.Serialize(dummyObj);
+
+			// Assert
+			Assert.AreEqual(serializedStr, "{\"variable\":\"value\"}");
+		}
+
+		[TestMethod]
+		public void Deserialize_Test()
+		{
+			// Arrange
+			DummyClass expectedDummyObj = new DummyClass();
+			expectedDummyObj.variable = "value";
+			string JSONStr = "{\"variable\":\"value\"}";
+
+			// Act
+			DummyClass dummyObj = Helper.Deserialize<DummyClass>(JSONStr);
+
+			// Assert
+			Assert.AreEqual(dummyObj.variable, expectedDummyObj.variable);
+		}
+	}
+}
